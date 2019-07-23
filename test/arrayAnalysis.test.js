9395a1f03b97f1d22b0cda3dd421a207baad20f3
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-07-23 22:30:36 +0300

    Add arrayAnalysis function tests

---
+++ test/arrayAnalysis.test.js
@@ -0,0 +1,27 @@
+const analyze = require('../src/arrayAnalysis');
+
+describe('analyze', () => {
+  test('average', () => {
+    expect(analyze([1, 8, 3, 4, 2, 6]).average).toEqual(4);
+    expect(analyze([8, 2]).average).toEqual(5);
+    expect(analyze([8]).average).toEqual(8);
+  });
+
+  test('min', () => {
+    expect(analyze([1, 8, 3, 4, 2, 6]).min).toEqual(5);
+    expect(analyze([8, 2]).min).toEqual(2);
+    expect(analyze([8]).min).toEqual(8);
+  });
+
+  test('max', () => {
+    expect(analyze([1, 8, 3, 4, 2, 6]).max).toEqual(8);
+    expect(analyze([8, 2]).max).toEqual(8);
+    expect(analyze([8]).max).toEqual(8);
+  });
+
+  test('length', () => {
+    expect(analyze([1, 8, 3, 4, 2, 6]).length).toEqual(6);
+    expect(analyze([8, 2]).length).toEqual(2);
+    expect(analyze([8]).length).toEqual(1);
+  });
+});
