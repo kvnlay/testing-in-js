dc18383cc6da4c041bd73ccab339df38f3aa9d7b
Musa Jabbaaru Ntege <cenajab@gmail.com>
2019-07-24 00:10:13 +0300

    Update caesarCipher tests

--- test/caesarCipher.test.js
+++ test/caesarCipher.test.js
@@ -3,13 +3,16 @@ const caesarCipher = require('../src/caesarCipher');
 describe('caesarCipher', () => {
   test('wrapping from z to a', () => {
     expect(caesarCipher('zoo', 1)).toMatch('app');
+    expect(caesarCipher('z', 1)).not.toMatch('A');
   });
 
   test('keeping the same case', () => {
     expect(caesarCipher('BinD', 1)).toMatch('CjoE');
+    expect(caesarCipher('BIND', 1)).toMatch('CJOE');
   });
 
   test('punctuation', () => {
     expect(caesarCipher('zoo! BinD', 1)).toMatch('app! CjoE');
+    expect(caesarCipher('! @.., "', 1)).toMatch('! @.., "');
   });
 });
